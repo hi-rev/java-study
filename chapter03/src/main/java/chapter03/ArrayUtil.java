f11f8ca6d4a99f0df692b3dbe6379edb9cb208a5
hi-rev <hirev05@gmail.com>
2022-12-22 13:20:15 +0900

    연습문제

--- chapter03/src/main/java/chapter03/ArrayUtil.java
+++ chapter03/src/main/java/chapter03/ArrayUtil.java
@@ -1,17 +1,41 @@
 package chapter03;
 
 public class ArrayUtil {
-
-	public static double[] intToDouble(int[] is)  {
-		double[] result = new double[is.length];
+	
+	// 1. int 배열 -> double 배열
+	public static double[] intToDouble(int[] source)  {
+		double[] result = new double[source.length];
 		
-		for (int i = 0; i < is.length; i++) {
-			result[i] = is[i];
+		for (int i = 0; i < source.length; i++) {
+			result[i] = (double) source[i];
 		}
 		return result;
 	}
-
+	
+	// 2. double 배열 -> int 배열
+	public static int[] doubleToInt(double[] source) {
+		int[] result = new int[source.length];
+		
+		for (int i = 0; i < source.length; i++) {
+			result[i] = (int) source[i];
+		}
+		return result;
+	}
+	
+	// 3. int 배열 연결
 	public static int[] concat(int[] is, int[] is2) {
-		return null;
+		int sumIdx = is.length + is2.length;
+		int[] result = new int[sumIdx];
+		
+		int idx = 0;
+		for (int n : is) {
+			result[idx++] = n;
+		}
+		
+		for (int n : is2) {
+			result[idx++] = n;
+		}
+		
+		return result;
 	}
 }
--- chapter03/src/main/java/chapter03/ArrayUtilTest.java
+++ chapter03/src/main/java/chapter03/ArrayUtilTest.java
@@ -15,11 +15,11 @@ public class ArrayUtilTest {
 		double[] d = ArrayUtil.intToDouble(new int[] {10, 20, 30, 40});
 		System.out.println(Arrays.toString(d));
 		
-		int[] i1 = ArrayUtil.concat(new int[] {1, 2, 3}, new int[] {4, 5, 6});
-//		System.out.println(Arrays.toString(i2));
+		int[] arrInt = ArrayUtil.doubleToInt(new double[] {10, 20, 30, 40});
+		System.out.println(Arrays.toString(arrInt));
 		
-		Goods g = new Goods();
-		System.out.println(g);
-		System.out.println(g.toString());
+		int[] arrConcat = ArrayUtil.concat(new int[] {1, 2, 3}, new int[] {4, 5, 6});
+		System.out.println(Arrays.toString(arrConcat));
+		
 	}
 }
