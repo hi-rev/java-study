ad0bd7e7231199f6d2cdff15268594e0e3bc1e09
hi-rev <hirev05@gmail.com>
2022-12-26 09:55:09 +0900

    generic method

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,19 +2,36 @@ package prob01;
 
 public class Printer {
 
-	public void println(int i) {
-		System.out.println(i);
+//	public void println(int i) {
+//		System.out.println(i);
+//	}
+//
+//	public void println(boolean b) {
+//		System.out.println(b);
+//	}
+//
+//	public void println(double d) {
+//		System.out.println(d);
+//	}
+//
+//	public void println(String string) {
+//		System.out.println(string);
+//	}
+	
+	// generic method
+	public <T> void println(T... ts) {
+		for (T t:ts) {
+			System.out.println(t);
+		}
+//		System.out.println(t);
 	}
-
-	public void println(boolean b) {
-		System.out.println(b);
-	}
-
-	public void println(double d) {
-		System.out.println(d);
-	}
-
-	public void println(String string) {
-		System.out.println(string);
+	
+	// 가변 파라미터
+	public int sum(Integer... nums) {
+		int s = 0;
+		for (Integer n: nums) {
+			s += n;
+		}
+		return s;
 	}
 }
