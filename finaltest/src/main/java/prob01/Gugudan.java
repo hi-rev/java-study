ade9f5871d30d7ded95fc840b491a87f0f8b5264
hi-rev <hirev05@gmail.com>
2022-12-27 13:56:54 +0900

    io #2

--- finaltest/src/main/java/prob01/Gugudan.java
+++ finaltest/src/main/java/prob01/Gugudan.java
@@ -1,6 +1,7 @@
 package prob01;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class Gugudan {
@@ -10,5 +11,29 @@ public class Gugudan {
 	public Gugudan(int lValue, int rValue) {
 		this.lValue = lValue;
 		this.rValue = rValue;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(lValue, rValue);
+	}
+
+	// 본래 Java에서 equals 메소드는 동등성 비교를 위한 것
+	// 여기에서는 구구단값의 동일성을 비교한다.
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Gugudan other = (Gugudan) obj;
+		return lValue * other.rValue == rValue * other.lValue;
+	}
+	
+	@Override
+	public String toString() {
+		return "Gugudan [lValue=" + lValue + ", rValue=" + rValue + "]";
 	}
 }
