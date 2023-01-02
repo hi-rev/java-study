b7e2d21c834ea2432483ba20c447539c72e325a9
nagyeong <nagyeong@localhost>
2023-01-03 00:57:29 +0900

    test

---
+++ network/src/main/java/test02/EchoClient.java
@@ -0,0 +1,66 @@
+package test02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoClient {
+	
+	private static final String SERVER_IP = "127.0.0.1";
+
+	public static void main(String[] args) {
+		Scanner sc = null;
+		
+		// 1. 소켓 생성
+		Socket socket = new Socket();
+		
+		try {
+			// 2. 서버 연결
+			socket.connect(new InetSocketAddress(SERVER_IP, EchoServer.PORT));
+			System.out.println("client connect");
+			
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true);
+			
+			sc = new Scanner(System.in);
+			while (true) {
+				System.out.print("> ");
+				String line = sc.nextLine();
+				
+				if ("exit".equals(line)) {
+					break;
+				}
+				pw.println(line);
+				
+				String data = br.readLine();
+				if (data == null) {
+					System.out.println("closed by server");
+					break;
+				}
+				System.out.println("나경: " + data);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+				try {
+					if (socket != null && !socket.isClosed()) {
+						socket.close();
+					}
+					if (sc != null) {
+						sc.close();
+					}
+						
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+
---
+++ network/src/main/java/test02/EchoThread.java
@@ -0,0 +1,62 @@
+package test02;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class EchoThread extends Thread {
+
+	private Socket socket;
+	
+	public EchoThread(Socket socket) {
+		
+		this.socket = socket;
+		
+	}
+
+	@Override
+	public void run() {
+		// 1. remote Host Information
+		InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
+		String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
+		int remotePort = inetRemoteSocketAddress.getPort();
+		System.out.println("connected by client[" + remoteHostAddress + " : " + remotePort + "]");
+		
+		try {
+			// 2. 스트림 얻기
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true);
+			
+			while (true) {
+				String data = br.readLine(); 
+				
+			if (data == null) {
+				System.out.println("closed by client");
+				break;
+			}
+			
+//			System.out.println("received: " + data);
+			pw.println(data);
+			
+			}
+			
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("error: " + e);
+		} finally {
+			if (socket != null && !socket.isClosed()) {
+				try {
+					socket.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
