e20a209126681e0c3ce9d2a6e03b02a4211793ae
hi-rev <hirev05@gmail.com>
2023-01-07 18:25:29 +0900

    과제: chat gui

--- network/src/main/java/chat/gui/ChatClientApp.java
+++ network/src/main/java/chat/gui/ChatClientApp.java
@@ -1,16 +1,26 @@
 package chat.gui;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import java.util.Scanner;
+
+import chat.ChatServer;
 
 public class ChatClientApp {
 
 	public static void main(String[] args) {
 		String name = null;
-		Scanner scanner = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
+		Socket socket = null;
 
 		while( true ) {
 			System.out.println("대화명을 입력하세요.");
 			System.out.print(">>> ");
-			name = scanner.nextLine();
+			name = sc.nextLine();
 			
 			if (!name.isEmpty()) {
 				break;
@@ -19,8 +29,37 @@ public class ChatClientApp {
 			System.out.println("대화명은 한글자 이상 입력해야 합니다.\n");
 		}
 		
-		scanner.close();
+		sc.close();
 		
-		new ChatWindow(name).show();
+		try {
+			// 1. create socket
+			socket = new Socket();
+			
+			// 2. connect to server
+			// -> chat.ChatServer 사용
+			socket.connect(new InetSocketAddress("0.0.0.0", ChatServer.PORT));
+			
+			// 3. get iostream
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true);
+			
+			// 4. join protocol 진행
+			pw.println("join:" + name);
+			String line = br.readLine();
+			if ("join:ok".equals(line)) {
+				new ChatWindow(name, socket).show();
+				return;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			try {
+				if(!socket.isClosed() && socket != null) {
+					socket.close();					
+				}
+			} catch (IOException es) {
+				es.printStackTrace();
+			}
+		} 
 	}
 }
+
--- network/src/main/java/chat/gui/ChatWindow.java
+++ network/src/main/java/chat/gui/ChatWindow.java
@@ -12,6 +12,14 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.Socket;
+import java.net.SocketException;
 
 public class ChatWindow {
 
@@ -20,83 +28,113 @@ public class ChatWindow {
 	private Button buttonSend;
 	private TextField textField;
 	private TextArea textArea;
-	private String name;
+	private Socket socket;
+	private BufferedReader br;
+	private PrintWriter pw = null;
 
-	public ChatWindow(String name) {
-		this.name = name;
+	public ChatWindow(String name, Socket socket) {
+		this.socket = socket;
 		frame = new Frame(name);
 		pannel = new Panel();
 		buttonSend = new Button("Send");
 		textField = new TextField();
 		textArea = new TextArea(30, 80);
-		new ChatClientThread().start();;
 	}
 
 	public void show() {
-		// Button
-		buttonSend.setBackground(Color.GRAY);
-		buttonSend.setForeground(Color.WHITE);
-		// 버튼을 누를 때 발생하는 이벤트
-		buttonSend.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				sendMessage();
-			}
-			
-		});
-		
-		// 버튼 누를 때 발생하는 이벤트 code2
-//		buttonSend.addActionListener((e) -> {
-//		});
-
-		// Textfield
-		textField.setColumns(80);
-		textField.addKeyListener(new KeyAdapter() {
-			// Enter 누르면 메세지 전송
-			@Override
-			public void keyPressed(KeyEvent e) {
-				char keyCode = e.getKeyChar();
-				if (keyCode == KeyEvent.VK_ENTER) {
+		try {
+			// Button
+			buttonSend.setBackground(Color.GRAY);
+			buttonSend.setForeground(Color.WHITE);
+			// 버튼을 누를 때 발생하는 이벤트
+			buttonSend.addActionListener(new ActionListener() {
+	
+				@Override
+				public void actionPerformed(ActionEvent e) {
 					sendMessage();
 				}
-			}
-		});
-
-		// Pannel
-		pannel.setBackground(Color.LIGHT_GRAY);
-		pannel.add(textField);
-		pannel.add(buttonSend);
-		frame.add(BorderLayout.SOUTH, pannel);
-
-		// TextArea
-		textArea.setEditable(false);
-		frame.add(BorderLayout.CENTER, textArea);
-
-		// Frame
-		frame.addWindowListener(new WindowAdapter() {
-			// frame 닫는거 구현
-			@Override
-			public void windowClosing(WindowEvent e) {
-				finish();
-			}
-		});
-		frame.setVisible(true);
-		frame.pack();
+				
+			});
+			
+			// 버튼 누를 때 발생하는 이벤트 code2
+	//		buttonSend.addActionListener((e) -> {
+	//		});
+	
+			// Textfield
+			textField.setColumns(80);
+			textField.addKeyListener(new KeyAdapter() {
+				// Enter 누르면 메세지 전송
+				@Override
+				public void keyPressed(KeyEvent e) {
+					char keyCode = e.getKeyChar();
+					if (keyCode == KeyEvent.VK_ENTER) {
+						sendMessage();
+					}
+				}
+			});
+	
+			// Pannel
+			pannel.setBackground(Color.LIGHT_GRAY);
+			pannel.add(textField);
+			pannel.add(buttonSend);
+			frame.add(BorderLayout.SOUTH, pannel);
+	
+			// TextArea
+			textArea.setEditable(false);
+			frame.add(BorderLayout.CENTER, textArea);
+	
+			// Frame
+			frame.addWindowListener(new WindowAdapter() {
+				// frame 닫는거 구현
+				@Override
+				public void windowClosing(WindowEvent e) {
+					finish();
+				}
+			});
+			frame.setVisible(true);
+			frame.pack();
+			
+			// IOStream 받아오기
+			br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
+			pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"),true);
+			
+			// ChatClientThread run!
+			new ChatClientThread().start();
+			
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} 
+//		finally {
+//			try {
+//				if(br != null) {
+//					br.close();
+//				}
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
+//		}
+// -> finish()에서 br.close()
 	}
 	
 	private void sendMessage() {
 		String message = textField.getText();
 		
-		if (message == "quit") {
+		// -> chat.ChatClinet에서 구현하였던 것과 동일하게 protocol 처리
+		if (message.equals("quit")) {
+			// quit 프로토콜 처리
+			pw.println("quit");
 			finish();
+		} else {
+			// 메시지 처리
+			pw.println("message:" + message);
 		}
 		
 		textField.setText(""); // 메세지창 비우기
 		textField.requestFocus(); // 메세지창 포커스
 		
 		// ChatClientThread 에서 서버로부터 받은 메세지가 있다 치고 
-		updateTextArea(name + ":" + message);
 	}
 	
 	private void updateTextArea(String message) {
@@ -107,14 +145,45 @@ public class ChatWindow {
 	private class ChatClientThread extends Thread {
 		@Override
 		public void run() {
-			// String message = br.readLine();
-			updateTextArea("즐거운 채팅 되세요!");
+			 try {
+				 // chat.ChatClientThread와 동일하게 구현해주면 됨
+				 while (true) {
+						String data = br.readLine();
+						System.out.println(data);
+						if (data == null) {
+							System.out.println("[client] closed");
+							break;
+						} 
+						// 메세지 보내기
+						updateTextArea(data);
+					}
+			 } catch (SocketException e) {
+					finish();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 	
 	// frame 종료하기
 	private void finish() {
-		// exit java(Application)
-		System.exit(0);
+		try {
+			// quit protocol 구현 (명시적으로 프로토콜로 종료해주는 것이 좋음)
+			pw.println("quit");
+			
+			// clean-up
+			if (socket != null & !socket.isClosed()) {
+				socket.close();
+			}
+			if(br != null) {
+				br.close();
+			}
+			
+			// exit java(Application)
+			System.exit(0);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
